a7d8aa317a48b1f14602e6794a840d856dbeef34
Jaiden King <king.jaiden@yahoo.com>
2013-10-17 08:55:50 -0700

    Slick

--- LWJGLTutorial/src/LWJGLTutorial.java
+++ LWJGLTutorial/src/LWJGLTutorial.java
@@ -5,6 +5,8 @@ import java.nio.FloatBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.lwjgl.opengl.GL11.*;
+
 import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.ContextAttribs;
@@ -38,10 +40,6 @@ public class LWJGLTutorial {
 		LWJGLTutorial test = new LWJGLTutorial();
 		test.start();
 	}
-	
-	public float degToRad(float degrees) {			//utility function, in case thinking in degrees is a bit easier
-		return (float) ((degrees / 180) * Math.PI);
-	}
 
 	public void start() {
 		
@@ -52,51 +50,19 @@ public class LWJGLTutorial {
 			e.printStackTrace();
 		}
 		
-		String glVersion = "1.1f";
-		float glVersionf = 1.1f;
-		
-		try {
-			glVersion = GL11.glGetString(GL11.GL_VERSION);
-			String[] glVersionSplit = glVersion.split("\\.");
-			glVersion = glVersionSplit[0] + "." + glVersionSplit[1];
-			glVersionf = Float.parseFloat(glVersion);
-			LWJGLTutorial.glVersionF = glVersionf;
-			System.out.println("The GL version for this computer is " + glVersion);
-		}
-		catch(NumberFormatException e) {
-			glVersion = "1.1f";
-			glVersionf = 1.1f;
-			e.printStackTrace();
-		}
-		
 		Display.destroy();
 		
-		if(glVersionf <= 3.1) {
-			try {
-				Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
-				Display.setTitle("LWJGL Tutorial");
-				Display.create();
-			} catch(LWJGLException ex) {
-				ex.printStackTrace();
-				System.exit(-1);
-			}
-		}
-		else if(glVersionf >= 3.2){
-			PixelFormat pixelFormat = new PixelFormat(8, 8, 0).withBitsPerPixel(32);
-			ContextAttribs attributes = new ContextAttribs(3, 2).withForwardCompatible(true).withProfileCore(true).withDebug(true);
-			try {
-				Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
-				Display.setTitle("LWJGL Tutorial");
-				Display.create(pixelFormat, attributes);
-			} catch(LWJGLException ex) {
-				ex.printStackTrace();
-				System.exit(-1);
-			}
+		try {
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.setTitle("LWJGL Tutorial");
+			Display.create();
+		} catch (LWJGLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 		
-		GL11.glViewport(0, 0, WIDTH, HEIGHT);
 		
-		initGL(glVersionf, WIDTH, HEIGHT);
+		initGL(WIDTH, HEIGHT);
 		
 		float[] firstVertices = {
 			0.2f, 0.4f, 0.0f,
@@ -139,18 +105,10 @@ public class LWJGLTutorial {
 			Display.update();
 			Display.sync(60);
 		}
-		
-		if(LWJGLTutorial.glVersionF >= 3.2) {
-			for(int i = 0; i < objects.size(); i++) {
-				objects.get(i).releaseMemory();
-			}
-			releaseGLMemory();
-		}
-		
 		Display.destroy();
 	}
 	
-	public void initGL(float glVersionF, int width, int height) {
+	public void initGL(int width, int height) {
 		
 		GL11.glClearColor(0.3f, 0.7f, 0.8f, 0.4f);
 		
@@ -163,53 +121,10 @@ public class LWJGLTutorial {
 			
 			GL11.glEnable(GL11.GL_TEXTURE_2D);
 		}
-		else {
-			projectionMatrix = new Matrix4f();
-			float aspectRatio = (float) (width/height);
-			float FOV = 80f;		//our field of view is the scale of the "back"
-			float near = 1f;
-			float far = -1f;
-			
-			float yScaled = (float) (1/(Math.tan((FOV / 2) * (Math.PI / 180)))); 	//sets up the scale of y at the "back"
-			float xScaled = yScaled / aspectRatio; 					//take the back height and divides it by the ratio which is still used, giving the width
-			float depth = far - near; 				//finds the depth, in this case 0.5f
-			
-			projectionMatrix.m00 = xScaled;						/* These variables are internally used by OpenGL and represent 		*/
-			projectionMatrix.m11 = yScaled;						/* different "positions" used by the projection matrix, or the 		*/
-			projectionMatrix.m22 = -((far + near) / depth);		/* sort of 3D trapezoid that represents what can be seen past  		*/
-			projectionMatrix.m23 = -1;							/* the "camera". Legacy OpenGL takes care of this automatic-   		*/
-			projectionMatrix.m32 = -((2 * near * far) / depth); /* ally using the functions seen above (glOrtho being the big one). */
-			projectionMatrix.m33 = 0;
-			
-			/* ((1/Math.tan((FOV/2) * (Math.PI/180)))/aspectRatio)	 	   0			0			0
-			 * 
-			 * 
-			 * 
-			 * 0			(1/(Math.tan((FOV/2) * (Math.PI/180))))						0			0
-			 * 
-			 * 
-			 * 
-			 * 0							0						-((far+near)/depth)				-1
-			 * 
-			 * 
-			 * 
-			 * 0							0						-2((2*near*far)/depth)			 0 
-			 */
-			
-			vertShaderID = getShader("shaders/vertShader.glsl", GL20.GL_VERTEX_SHADER);
-			fragShaderID = getShader("shaders/colorShader.glsl", GL20.GL_FRAGMENT_SHADER);
-			shaderProgramID = GL20.glCreateProgram();
-			GL20.glAttachShader(shaderProgramID, vertShaderID);
-			GL20.glAttachShader(shaderProgramID, fragShaderID);
-			GL20.glBindAttribLocation(shaderProgramID, 0, "in_Position");
-            GL20.glBindAttribLocation(shaderProgramID, 1, "in_Color");
-			GL20.glLinkProgram(shaderProgramID);
-			GL20.glValidateProgram(shaderProgramID);
-		}
 	}
 	
 	public void renderObjects(List<OpenGL_Object> objects) {
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+		glDisable(GL_TEXTURE_2D);
 		for(int i = 0; i < objects.size(); i++) {
 			objects.get(i).draw();
 		}
@@ -222,52 +137,13 @@ public class LWJGLTutorial {
 	}
 	
 	public void render(List<OpenGL_Object> objects, List<PNG_Texture> pngs) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glLoadIdentity();
 		if(objects.size() != 0) {
 			renderObjects(objects);
 		}
 		if(pngs.size() != 0) {
 			renderPNGs(pngs);
 		}
-	}
-	
-	public int getShader(String path, int shaderType) {
-		StringBuilder source = new StringBuilder();
-		int shadersID = 0;
-		
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(path));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				source.append(line).append("\n");
-			}
-			reader.close();
-		}
-		catch(IOException e) {
-			System.err.println("Couldn't read the file");
-			e.printStackTrace();
-			System.exit(-1);
-		}
-		
-		shadersID = GL20.glCreateShader(shaderType);
-		GL20.glShaderSource(shadersID, source);
-		GL20.glCompileShader(shadersID);
-		System.out.println(GL20.glGetShaderInfoLog(shadersID, 245));
-		
-		return shadersID;
-	}
-	
-	public void releaseGLMemory() {
-		GL20.glUseProgram(0);
-		GL20.glDetachShader(shaderProgramID, vertShaderID);
-		GL20.glDetachShader(shaderProgramID, fragShaderID);
-		GL20.glDeleteShader(vertShaderID);
-		GL20.glDeleteShader(fragShaderID);
-		GL20.glDeleteProgram(shaderProgramID);
-		GL30.glBindVertexArray(vaoID);
-		GL20.glDisableVertexAttribArray(0);
-		GL20.glDisableVertexAttribArray(1);
-		
-		GL30.glBindVertexArray(0);
-		GL30.glDeleteVertexArrays(vaoID);
 	}
 }
--- LWJGLTutorial/src/OpenGL_Object.java
+++ LWJGLTutorial/src/OpenGL_Object.java
@@ -14,20 +14,11 @@ import org.lwjgl.util.glu.GLU;
 
 public class OpenGL_Object {
 	
-	private float glVers;
 	private int screenWidth = LWJGLTutorial.WIDTH;	//for use with translation and rotation
 	private int screenHeight = LWJGLTutorial.HEIGHT;
 	
 	private float[] vertices;
 	private float[] colors;
-	
-	private int vaoID = LWJGLTutorial.vaoID;	//vertex array object
-	private int vboID = 0;	//vertex buffer object
-	private int vboiID = 0;	//vertex buffer object index
-	private int vbocID = 0;	//vertex buffer object color
-	private int indexCount = 0;
-	private int shaderProgramID = LWJGLTutorial.shaderProgramID;
-	private byte[] indices;
 	
 	private float adjustedWidth = (float)(screenWidth/2);
 	private float adjustedHeight = (float)(screenHeight/2);
@@ -36,106 +27,20 @@ public class OpenGL_Object {
 	FloatBuffer colorBuffer;
 
 	public OpenGL_Object(float[] vertices, float[] colors) {
-		glVers = LWJGLTutorial.glVersionF;
 		this.vertices = vertices;
 		this.colors = colors;
-		
-		indices = new byte[(vertices.length/3)];
-		for(byte i = 0; i < indices.length; i++) {
-			indices[i] = i;
-		}
-		
-		if(glVers >= 3.2) {
-			
-			int errorValue = GL11.glGetError();
-			if(errorValue != GL11.GL_NO_ERROR) {
-				System.out.println(GLU.gluErrorString(errorValue));
-				System.exit(-1);
-			}
-			
-			int projMatID = GL20.glGetUniformLocation(shaderProgramID, "LWJGLTutorial.projectionMatrix");
-			
-			verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
-			verticesBuffer.put(vertices);
-			verticesBuffer.flip();
-			
-			colorBuffer = BufferUtils.createFloatBuffer(colors.length);
-			colorBuffer.put(colors);
-			colorBuffer.flip();
-			
-			indexCount = (indices.length);
-			ByteBuffer indicesBuffer = BufferUtils.createByteBuffer(indexCount);
-			indicesBuffer.put(indices);
-			indicesBuffer.flip();
-		
-			vaoID = GL30.glGenVertexArrays();
-			GL30.glBindVertexArray(vaoID);
-		
-			vboID = GL15.glGenBuffers();
-			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
-			GL15.glBufferData(GL15.GL_ARRAY_BUFFER, verticesBuffer, GL15.GL_STATIC_DRAW);
-			GL20.glEnableVertexAttribArray(0);
-			GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
-			
-			vbocID = GL15.glGenBuffers();
-			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbocID);
-			GL15.glBufferData(GL15.GL_ARRAY_BUFFER, colorBuffer, GL15.GL_STATIC_DRAW);
-			GL20.glEnableVertexAttribArray(1);
-			GL20.glVertexAttribPointer(1, 3, GL11.GL_FLOAT, false, 0, 0);
-			
-			vboiID = GL15.glGenBuffers();
-			GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiID);
-			GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL15.GL_STATIC_DRAW);
-			
-			GL30.glBindVertexArray(0);
-			
-			GL20.glUseProgram(shaderProgramID);
-			LWJGLTutorial.projectionMatrix.store(LWJGLTutorial.projMatBuffer);
-			LWJGLTutorial.projMatBuffer.flip();
-			GL20.glUniformMatrix4(projMatID, false, LWJGLTutorial.projMatBuffer);
-			GL20.glUseProgram(0);
-		}
 	}
 	
 	public void draw() {
-		if(glVers <= 3.1) {
-			GL11.glBegin(GL11.GL_TRIANGLES);
-				GL11.glColor3f(colors[0], colors[1], colors[2]);
-				GL11.glVertex3f((vertices[0] * adjustedWidth), (vertices[1] * adjustedHeight), vertices[2]);
-				
-				GL11.glColor3f(colors[3], colors[4], colors[5]);
-				GL11.glVertex3f((vertices[3] * adjustedWidth), (vertices[4] * adjustedHeight), vertices[5]);
-				
-				GL11.glColor3f(colors[6], colors[7], colors[8]);
-				GL11.glVertex3f((vertices[6] * adjustedWidth), (vertices[7] * adjustedHeight), vertices[8]);
-			GL11.glEnd();
-		}
-		else if(glVers >= 3.2) {
+		GL11.glBegin(GL11.GL_TRIANGLES);
+			GL11.glColor3f(colors[0], colors[1], colors[2]);
+			GL11.glVertex3f((vertices[0] * adjustedWidth), (vertices[1] * adjustedHeight), vertices[2]);
 			
-			GL20.glUseProgram(shaderProgramID);
+			GL11.glColor3f(colors[3], colors[4], colors[5]);
+			GL11.glVertex3f((vertices[3] * adjustedWidth), (vertices[4] * adjustedHeight), vertices[5]);
 			
-			GL30.glBindVertexArray(vaoID);
-			GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiID);
-			GL11.glDrawElements(GL11.GL_TRIANGLES, indexCount, GL11.GL_UNSIGNED_BYTE, 0);
-			
-			GL20.glUseProgram(0);
-		}
-	}
-	
-	public void rotate(float z) {
-		
-	}
-	
-	public void translate(float x, float y) {
-		//can be implemented by just adding to "vertices" and then redoing the buffer stuff
-	}
-	
-	public void releaseMemory() {
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-		GL15.glDeleteBuffers(vboID);
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-		GL15.glDeleteBuffers(vbocID);
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
-		GL15.glDeleteBuffers(vboiID);
+			GL11.glColor3f(colors[6], colors[7], colors[8]);
+			GL11.glVertex3f((vertices[6] * adjustedWidth), (vertices[7] * adjustedHeight), vertices[8]);
+		GL11.glEnd();
 	}
 }
